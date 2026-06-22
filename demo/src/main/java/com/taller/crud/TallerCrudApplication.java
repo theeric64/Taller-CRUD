ec3acb5b30b39cc0bf6c84c34758a857f2bedbec
Sowaci <hercifil@gmail.com>
2026-06-22 16:02:12 -0500

    Ingreso de los datos SQL y tercera prueba

--- demo/src/main/java/com/taller/crud/TallerCrudApplication.java
+++ demo/src/main/java/com/taller/crud/TallerCrudApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TallerCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TallerCrudApplication.class, args);
+    }
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.taller.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TallerCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de Spring se cargue correctamente
+    }
 }
